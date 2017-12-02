3f892529fba6fb3671ca609158b9d79551c8324f
DanGiaime <dsg6874@g.rit.edu>
2017-12-01 23:28:42 -0500

    textures from spritesheet

--- js/main.js
+++ js/main.js
@@ -21,6 +21,7 @@ let circles = [];
 let bullets = [];
 let aliens = [];
 let explosions = [];
+let states = [];
 let explosionTextures;
 let score = 0;
 let life = 100;
@@ -30,52 +31,54 @@ let paused = true;
 function setup()
 {
     stage = app.stage;
-    
+
     // #1 - Create the 'start' scene
     startScene = new PIXI.Container();
     stage.addChild(startScene);
-    
+
     // #2 - Create the main 'game' scene and make it invisible
     gameScene = new PIXI.Container();
     gameScene.visible = false;
     stage.addChild(gameScene);
-    
+
     // #3 - Create the 'gameOver' scene and make it invisible
     gameOverScene = new PIXI.Container();
     gameOverScene.visible = false;
     stage.addChild(gameOverScene);
-    
+
     // #4 - Create labels for all 3 scenes
     createLabelsAndButtons();
-    
+
     // #5 - Create ship
-    ship = new Ship();
-    gameScene.addChild(ship);
-    
+    // ship = new Ship();
+    // gameScene.addChild(ship);
+
     // #6 - Load Sounds
     shootSound = new Howl({
-        src: ['sounds/shoot.wav'] 
+        src: ['sounds/shoot.wav']
     });
-    
+
     hitSound = new Howl({
-        src: ['sounds/hit.mp3'] 
+        src: ['sounds/hit.mp3']
     });
-    
+
     fireballSound = new Howl({
-        src: ['sounds/fireball.mp3'] 
+        src: ['sounds/fireball.mp3']
     });
-    
+
     // #7 - Load sprite sheet
     explosionTextures = loadSpriteSheet();
-    
+    states = loadSpriteSheetStates();
+
     // #8 - Start update loop
     app.ticker.add(gameLoop);
-    
+
     // #9 - Start listening for click events on the canvas
     app.view.onclick = fireBullet;
-    
+
     // Now our 'startScene' is visible
     // Clicking the button calls startGame()
+    console.log(states);
 }
 
 function createLabelsAndButtons()
@@ -85,7 +88,7 @@ function createLabelsAndButtons()
         fontSize: 48,
         fontFamily: "Futura"
     });
-    
+
     // 1 - set up 'startScene'
     // 1A - make the top start label
     let startLabel1 = new PIXI.Text("Circle Blast!");
@@ -99,7 +102,7 @@ function createLabelsAndButtons()
     startLabel1.x = 50;
     startLabel1.y = 120;
     startScene.addChild(startLabel1);
-    
+
     // 1B - make the middle start label
     let startLabel2 = new PIXI.Text("R U worthy..?");
     startLabel2.style = new PIXI.TextStyle({
@@ -113,7 +116,7 @@ function createLabelsAndButtons()
     startLabel2.x = 185;
     startLabel2.y = 300;
     startScene.addChild(startLabel2);
-    
+
     // 1C - make the start game button
     let startButton = new PIXI.Text("Enter, ... if you dare!");
     startButton.style = buttonStyle;
@@ -125,7 +128,7 @@ function createLabelsAndButtons()
     startButton.on('pointerover', e=>e.target.alpha = 0.7);
     startButton.on('pointerout', e=>e.currentTarget.alpha = 1.0);
     startScene.addChild(startButton);
-    
+
     // 2 - set up 'gameScene'
     let textStyle = new PIXI.TextStyle({
         fill: 0xFFFFFF,
@@ -134,7 +137,7 @@ function createLabelsAndButtons()
         stroke: 0xFF0000,
         strokeThickness: 4
     });
-    
+
     // 2A - make score label
     scoreLabel = new PIXI.Text();
     scoreLabel.style = textStyle;
@@ -142,7 +145,7 @@ function createLabelsAndButtons()
     scoreLabel.y = 5;
     gameScene.addChild(scoreLabel);
     increaseScoreBy(0);
-    
+
     // 2B - make life label
     lifeLabel = new PIXI.Text();
     lifeLabel.style = textStyle;
@@ -150,7 +153,7 @@ function createLabelsAndButtons()
     lifeLabel.y = 26;
     gameScene.addChild(lifeLabel);
     decreaseLifeBy(0);
-    
+
     // 3 - set up 'gameOverScene'
     // 3A - make game over text
     let gameOverText = new PIXI.Text("Game Over!\n          :-O");
@@ -165,7 +168,7 @@ function createLabelsAndButtons()
     gameOverText.x = 100;
     gameOverText.y = sceneHeight/2 - 160;
     gameOverScene.addChild(gameOverText);
-    
+
     // 3B - make "play again?" button
     let playAgainButton = new PIXI.Text("Play Again?");
     playAgainButton.style = buttonStyle;
@@ -177,14 +180,14 @@ function createLabelsAndButtons()
     playAgainButton.on('pointerover', e=>e.target.alpha = .7);
     playAgainButton.on('pointerout', e=>e.currentTarget.alpha = 1.0);
     gameOverScene.addChild(playAgainButton);
-    
+
     // 4 - create a game over score display
     gameOverScoreLabel = new PIXI.Text();
     gameOverScoreLabel.style = textStyle;
     gameOverScoreLabel.x = 50;
     gameOverScoreLabel.y = sceneHeight/2 + 80;
     gameOverScene.addChild(gameOverScoreLabel);
-    
+
 }
 
 function createExplosion(x,y,frameWidth, frameHeight)
@@ -246,17 +249,17 @@ function createCircles(numCircles)
 function fireBullet(e)
 {
     if(paused) return;
-    
+
     if(score >= 5)
     {
         let b1 = new Bullet(0xFFFFFF, ship.x + 10, ship.y);
         let b2 = new Bullet(0xFFFFFF, ship.x, ship.y);
         let b3 = new Bullet(0xFFFFFF, ship.x - 10, ship.y);
-        
+
         bullets.push(b1);
         bullets.push(b2);
         bullets.push(b3);
-        
+
         gameScene.addChild(b1);
         gameScene.addChild(b2);
         gameScene.addChild(b3);
@@ -267,7 +270,7 @@ function fireBullet(e)
         bullets.push(b);
         gameScene.addChild(b);
     }
-    shootSound.play();    
+    shootSound.play();
 }
 
 function loadLevel()
@@ -293,51 +296,74 @@ function loadSpriteSheet()
     return textures;
 }
 
+function loadSpriteSheetStates()
+{
+    // 16 animation frames are 64x64
+    // second row
+    let spriteSheet = PIXI.BaseTexture.fromImage("images/us_states.png");
+    let width = 64;
+    let height = 64;
+    let statesPerRow = 9;
+    let startState = 2;
+    let numRows = 6;
+    //let numFrames = 50;
+    let states = [];
+    for(let i = 0; i < numRows; i++)
+    {
+      for (let j = startState; j < statesPerRow; j++) {
+        let state = new PIXI.Texture(spriteSheet, new PIXI.Rectangle(j*width, i*height, width, height));
+        states.push(state);
+      }
+      startState = 0;
+    }
+    return states;
+}
+
 function end()
 {
     paused = true;
-    
+
     // clear out level
     circles.forEach(c=>gameScene.removeChild(c));
     circles = [];
-    
+
     bullets.forEach(b=>gameScene.removeChild(b));
     bullets = [];
-    
+
     explosions.forEach(e=>gameScene.removeChild(e));
     explosions = [];
-    
+
     gameOverScoreLabel.text = 'Your Final Score: ' + score;
-    
+
     gameOverScene.visible = true;
     gameScene.visible = false;
 }
 
 function gameLoop(){
-	
+
     if (paused) return;
-	
+
 	// #1 - Calculate "delta time"
     let dt = 1/app.ticker.FPS;
     if (dt > 1/12) dt=1/12;
-	
-	
+
+
 	// #2 - Move Ship
     let mousePosition = app.renderer.plugins.interaction.mouse.global;
-    //ship.position = mousePosition;  
-	
+    //ship.position = mousePosition;
+
     let amt = 6 * dt;   // at 60 fps would move about 10% of distance per update
-	
+
     // lerp (linear interpolate) the x & y with lerp
     let newX = lerp(ship.x, mousePosition.x, amt);
     let newY = lerp(ship.y, mousePosition.y, amt);
-    
+
     // keep the ship on the screen with clamp()
     let w2 = ship.width/2;
     let h2 = ship.height/2;
     ship.x = clamp(newX, 0 + w2, sceneWidth - w2);
     ship.y = clamp(newY, 0 + h2, sceneWidth - h2);
-    
+
 	// #3 - Move Circles
 	for (let c of circles)
     {
@@ -347,20 +373,20 @@ function gameLoop(){
             c.reflectX();
             c.move(dt);
         }
-        
+
         if(c.y <= c.radius || c.y >= sceneHeight - c.radius)
         {
             c.reflectY();
             c.move(dt);
         }
     }
-	
+
 	// #4 - Move Bullets
     for(let b of bullets)
     {
         b.move(dt);
     }
-	
+
 	// #5 - Check for Collisions
 	for (let c of circles)
     {
@@ -387,25 +413,25 @@ function gameLoop(){
             decreaseLifeBy(20);
         }
     }
-	
+
 	// #6 - Now do some clean up
-	
+
     // get rid of dead bullets
 	bullets = bullets.filter(b=>b.isAlive);
-    
+
     // get rid of dead circles
     circles = circles.filter(c=>c.isAlive);
-    
+
     // get rid of explosions
     explosions = explosions.filter(e=>e.playing);
-    
+
 	// #7 - Is game over?
 	if(life <= 0)
     {
         end();
         return; // return here so we skip #8 below
     }
-	
+
 	// #8 - Load next level
     if (circles.length == 0)
     {
